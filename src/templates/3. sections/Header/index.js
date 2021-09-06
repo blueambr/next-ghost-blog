f44da8c457272370509fd5e64a65856a264f28cc
Vlad Gerasimovich <hotepp@pm.me>
2021-09-06 22:24:29 +0200

    feat: update the header a bit, add hide/show button

--- src/templates/3. sections/Header/index.js
+++ src/templates/3. sections/Header/index.js
@@ -1,20 +1,62 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import Logo from '@/elements/Logo';
 import Nav from '@/components/Nav';
 import styles from './Header.module.scss';
 
+const Trigger = ({ classname, icon, isHidden, setIsHidden, title }) => (
+  <div className={`${styles.trigger} ${classname || ''}`}>
+    <button
+      className={styles.trigger__button}
+      type="button"
+      title={title}
+      onClick={() => setIsHidden(!isHidden)}
+    >
+      <div className={styles['trigger__button-icon']}>
+        <FontAwesomeIcon icon={icon} />
+      </div>
+    </button>
+  </div>
+);
+
 const Header = ({ data }) => {
   const { title, navigation } = data;
+  const trigger = {
+    showTitle: 'Show the header',
+    hideTitle: 'Hide the header',
+  };
+
+  const [isHidden, setIsHidden] = useState(false);
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isHidden ? styles.header_hidden : ''}`}>
       <div className="container">
         <div className={styles.wrapper}>
-          <div className={styles.logo}>
-            <Logo data={title} />
+          <div className={styles.content}>
+            <div className={styles.logo}>
+              <Logo data={title} />
+            </div>
+            <div className={styles.nav}>
+              <Nav data={navigation} />
+            </div>
           </div>
-          <div className={styles.nav}>
-            <Nav data={navigation} />
-          </div>
+          {!isHidden ? (
+            <Trigger
+              icon={faEyeSlash}
+              isHidden={isHidden}
+              setIsHidden={setIsHidden}
+              title={trigger.hideTitle}
+            />
+          ) : (
+            <Trigger
+              classname={styles.trigger_show}
+              icon={faEye}
+              isHidden={isHidden}
+              setIsHidden={setIsHidden}
+              title={trigger.showTitle}
+            />
+          )}
         </div>
       </div>
     </header>
