8d9a5dc53c0be548b06c619867fe5ebbd00e2b66
Vlad Gerasimovich <hotepp@pm.me>
2021-09-07 22:10:59 +0200

    wip: update media queries, add react-responsive, update header behaviour

--- src/contexts/SidebarVisibility.js
+++ src/contexts/SidebarVisibility.js
@@ -1,9 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const SidebarVisibilityContext = createContext(false, null);
 
 export const SidebarVisibilityProvider = ({ children }) => {
   const [isSidebarHidden, setIsSidebarHidden] = useState(false);
+
+  const isTouch = useMediaQuery({ query: '(max-width: 1024px)' });
+
+  // We only want to set this once: when the website is loaded/reloaded
+  useEffect(() => {
+    setIsSidebarHidden(!!isTouch);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <SidebarVisibilityContext.Provider value={[isSidebarHidden, setIsSidebarHidden]}>
